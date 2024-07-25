3acdb66f004a7f79782b0eaa9b0de509d285deec
Markus Flambard <markus.flambard@youcal.com>
2024-07-25 19:50:57 +0200

    Add function go_modem:query/2

--- src/go_modem.erl
+++ src/go_modem.erl
@@ -1,6 +1,20 @@
 -module(go_modem).
 
--export([start_connection/3]).
+-export([
+    start_connection/3,
+    query/2
+]).
+
+-type query() :: atom().
+-type answer() :: term().
+
+-spec start_connection(IoDevice :: term(), Server :: pid(), ServerMod :: module()) ->
+    {ok, pid()} | {error, Reason :: term()}.
 
 start_connection(IoDevice, Server, ServerMod) ->
     go_modem_sup:start_connection(IoDevice, Server, ServerMod).
+
+-spec query(Connection :: pid(), Query :: query()) -> {ok, answer()} | {error, term()}.
+
+query(Connection, Query) ->
+    go_modem_connection:send_command(Connection, {query, Query}).
